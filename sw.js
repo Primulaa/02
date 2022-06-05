48e51be70e1c071dce6443563e9ae9cd66b22578
Primulaa <94114990+Primulaa@users.noreply.github.com>
2022-06-05 11:52:42 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -14,3 +14,24 @@ window.addEventListener('load', e => {
       document.querySelector('.alert').removeAttribute('hidden'); (5)
     }
   }
+
+  self.addEventListener('install', async event => {
+    console.log('install event')
+  });
+  
+  self.addEventListener('fetch', async event => {
+    console.log('fetch event')
+  });
+
+  const cacheName = 'pwa-conf-v1';
+  const staticAssets = [
+    '/02/',
+    '/02/index.html',
+    '/02/app.js',
+    '/02/styles.css'
+  ];
+
+  self.addEventListener('install', async event => {
+    const cache = await caches.open(cacheName); (1)
+    await cache.addAll(staticAssets); (2)
+  });
