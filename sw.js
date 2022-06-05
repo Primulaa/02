2041aba5c66169dab25d6f2cb6473d2ff9128715
Primulaa <94114990+Primulaa@users.noreply.github.com>
2022-06-05 12:21:07 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,10 @@
-const staticCacheName = 'site-static-v1';
-const assets = [
-  '/02/',
-  '/02/index.html',
-  '/02/style.css',
-  'https://fonts.googleapis.com/css?family=Lato:300,400,700',
-];
-// событие install
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      console.log('caching shell assets');
-      cache.addAll(assets);
-    })
-  );
-});
+const dynamicCacheName = 'site-dynamic-v1';
 // событие activate
 self.addEventListener('activate', evt => {
   evt.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(keys
-        .filter(key => key !== staticCacheName)
+        .filter(key =>  key !== dynamicCacheName)
         .map(key => caches.delete(key))
       );
     })
@@ -29,7 +14,12 @@ self.addEventListener('activate', evt => {
 self.addEventListener('fetch', evt => {
   evt.respondWith(
     caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request);
+      return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(dynamicCacheName).then(cache => {
+          cache.put(evt.request.url, fetchRes.clone());
+          return fetchRes;
+        })
+      });
     })
   );
 });
