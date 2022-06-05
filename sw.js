3ec9d40071c97a15d20b78b481f9f070fdecbb72
Primulaa <94114990+Primulaa@users.noreply.github.com>
2022-06-05 11:56:29 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -35,3 +35,42 @@ window.addEventListener('load', e => {
     const cache = await caches.open(cacheName); (1)
     await cache.addAll(staticAssets); (2)
   });
+
+  self.addEventListener('fetch', event => {
+    const req = event.request;
+    event.respondWith(cacheFirst(req));
+  });
+
+  async function cacheFirst(req) {
+    const cache = await caches.open(cacheName); (1)
+    const cachedResponse = await cache.match(req); (2)
+    return cachedResponse || fetch(req); (3)
+  }
+
+  self.addEventListener('fetch', event => {
+    const req = event.request;
+  
+    if (/.*(json)$/.test(req.url)) {
+      event.respondWith(networkFirst(req));
+    } else {
+      event.respondWith(cacheFirst(req));
+    }
+  });
+
+  async function networkFirst(req) {
+    const cache = await caches.open(cacheName);
+    try { (1)
+      const fresh = await fetch(req);
+      cache.put(req, fresh.clone());
+      return fresh;
+    } catch (e) { (2)
+      const cachedResponse = await cache.match(req);
+      return cachedResponse;
+    }
+  }
+
+  async function cacheFirst(req) {
+    const cache = await caches.open(cacheName);
+    const cachedResponse = await cache.match(req);
+    return cachedResponse || networkFirst(req);
+  }
